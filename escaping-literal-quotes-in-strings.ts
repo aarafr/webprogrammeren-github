e3b6e451bb71dc00dd99574de30085d522f49adb
Aaraf Rahman <aaraf.rahman@ap.be>
2023-01-30 15:38:42 +0100

    Excersise of quotes

---
+++ escaping-literal-quotes-in-strings.ts
@@ -0,0 +1,6 @@
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+let rStr: string = `I am a "double quoted" string inside "double quotes".`;
+
+console.log(myStr);
+console.log(rStr);
